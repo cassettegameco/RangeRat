0c008ee1f8bd8a46c791eb73aca57626ef1bd4d1
Tianna Henry-Lewis <tianna_lewis05@hotmail.com>
2026-05-30 23:43:43 -0400

    WEEK 1 - Render Background

--- source/main.lua
+++ source/main.lua
@@ -1,8 +1,19 @@
 import "CoreLibs/graphics"
+import "CoreLibs/sprites"
 
 local pd = playdate
 local gfx = pd.graphics
 
-function pd.update() 
-    gfx.drawTextAligned("Range Rat", 200, 40, kTextAlignment.center)
+-- ---------- BACKGROUND ----------
+local rangeBgImage = gfx.image.new("images/background_range")
+gfx.sprite.setBackgroundDrawingCallback(
+    function( x, y, width, height )
+        -- x,y,width,height is the updated area in sprite-local coordinates
+        -- The clip rect is already set to this area, so we don't need to set it ourselves
+        rangeBgImage:draw( 0, 0 )
+    end
+)
+
+function pd.update()
+    gfx.sprite.update()
 end
