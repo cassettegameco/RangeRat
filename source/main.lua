838546a9b0617ff4e8ff9d026f56c1d3741de6c3
Tianna Henry-Lewis <tianna_lewis05@hotmail.com>
2026-06-24 23:06:56 -0400

    bezier curve experimentation

--- source/main.lua
+++ source/main.lua
@@ -11,6 +11,7 @@ local tempoQuality = "BAD" -- ⚠️ make this a table
 
 -- ---------- BACKGROUND ----------
 local rangeBgImage = gfx.image.new("images/rangev3")
+--[[
 gfx.sprite.setBackgroundDrawingCallback(
     function( x, y, width, height )
         -- x,y,width,height is the updated area in sprite-local coordinates
@@ -18,6 +19,27 @@ gfx.sprite.setBackgroundDrawingCallback(
         rangeBgImage:draw( 0, 0 )
     end
 )
+]]
+
+-- ---------- BEZIER CURVE EXPERIMENTATION ----------
+local pointA = { x = 200, y = 220 }
+local pointB = { x = 380, y = 20 }
+local pointC = { x = 200, y = 120 }
+
+local function lerp(a, b, t)
+    return a + (b - a) * t
+end
+
+local function mix(a, b, t)
+    return {
+        x = lerp(a.x, b.x, t),
+        y = lerp(a.y, b.y, t)
+    }
+end
+
+local function bezier(a, b, c, t)
+    return mix(mix(a, c, t), mix(c, b, t), t)
+end
 
 -- ---------- SHOT ----------
 local swingState = "READY"
@@ -29,6 +51,11 @@ local isDownswing = false
 
 -- ---------- GAME LOOP ----------
 function pd.update()
+    gfx.clear()
+
+    local time = pd.getCurrentTimeMilliseconds() / 1000
+    local t = math.sin(time) * 0.5 + 0.5
+
     gfx.sprite.update()
 
     local crankPosition = pd.getCrankPosition() -- club/swing direction or aim angle
@@ -46,6 +73,47 @@ function pd.update()
     elseif smoothedSpeed >= 8 then
         tempoQuality = "FAST"
     end
+
+    -- ---------- Bezier Curve Experimation - START ----------
+    gfx.drawCircleAtPoint(pointA.x, pointA.y, 10)
+    gfx.drawCircleAtPoint(pointB.x, pointB.y, 10)
+    gfx.drawCircleAtPoint(pointC.x, pointC.y, 10)
+
+    local pointAC = mix(pointA, pointC, t)
+    local pointCB = mix(pointC, pointB, t)
+    gfx.drawCircleAtPoint(pointAC.x, pointAC.y, 7)
+    gfx.drawCircleAtPoint(pointCB.x, pointCB.y, 7)
+
+    local pointACB = mix(pointAC, pointCB, t)
+    gfx.drawCircleAtPoint(pointACB.x, pointACB.y, 7)
+
+    gfx.drawLine(pointA.x, pointA.y, pointC.x, pointC.y)
+    gfx.drawLine(pointC.x, pointC.y, pointB.x, pointB.y)
+    gfx.drawLine(pointAC.x, pointAC.y, pointCB.x, pointCB.y)
+
+    -- Approximate curve with filled circles drawing a dotted curve
+    local previousPoint = pointA
+    local newPoint = pointA
+    for i = 1, 25, 1 do
+        t = i/25
+
+        newPoint = bezier(pointA, pointB, pointC, t)
+
+        gfx.fillCircleAtPoint(newPoint.x, newPoint.y, 5)
+
+        previousPoint = newPoint
+    end
+
+    if pd.buttonIsPressed(pd.kButtonUp) then
+        pointC.y -= 1
+    elseif pd.buttonIsPressed(pd.kButtonRight) then
+        pointC.x += 1
+    elseif pd.buttonIsPressed(pd.kButtonDown) then
+        pointC.y += 1
+    elseif pd.buttonIsPressed(pd.kButtonLeft) then
+        pointC.x -= 1
+    end
+    -- ---------- Bezier Curve Experimation - END ----------
 
     -- ⚠️ replace HUD with a debug scene
     if showCrankHUD == true then
@@ -69,9 +137,7 @@ function pd.update()
 
         gfx.drawRect(meterX, meterY, meterW, meterH)
         local fillW = math.min(smoothedSpeed * 10, meterW)
-        -- gfx.setColor(gfx.kColorWhite)
         gfx.fillRect(meterX, meterY, fillW, meterH)
-        -- gfx.setColor(gfx.kColorBlack)
 
     end
 
